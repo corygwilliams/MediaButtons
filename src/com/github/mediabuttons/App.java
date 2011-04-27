8a399ddda0c0d1347d2de7a29b5a595df764272b
Cory Williams <corygwilliams@gmail.com>
2011-04-27 17:02:46 -0400

    Adding copyright headers.  Using a custom Application object to make sure the Repeater gets started if the process is restarted.

---
+++ src/com/github/mediabuttons/App.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2011 Cory Williams
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.mediabuttons;
+
+import android.app.Application;
+
+/**
+ * We override Application so that we can start the Repeater when the process
+ * is restarted.  We can't use static initializer since we need a Context.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Repeater.start(getApplicationContext());
+    }
+}
